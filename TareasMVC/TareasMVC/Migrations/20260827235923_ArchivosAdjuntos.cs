fe1146dc8e62c8fbd863bc401afd17e295b68626
Edgar Gonzalez <gzzenit@gmail.com>
2026-08-27 18:03:18 -0600

    archivo adjuntos

---
+++ TareasMVC/TareasMVC/Migrations/20260827235923_ArchivosAdjuntos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArchivosAdjuntos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ArchivosAdjuntos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TareaId = table.Column<int>(type: "integer", nullable: false),
+                    Url = table.Column<string>(type: "text", unicode: false, nullable: true),
+                    Titulo = table.Column<string>(type: "text", nullable: true),
+                    Orden = table.Column<int>(type: "integer", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArchivosAdjuntos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ArchivosAdjuntos_Tareas_TareaId",
+                        column: x => x.TareaId,
+                        principalTable: "Tareas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArchivosAdjuntos_TareaId",
+                table: "ArchivosAdjuntos",
+                column: "TareaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArchivosAdjuntos");
+        }
+    }
+}
